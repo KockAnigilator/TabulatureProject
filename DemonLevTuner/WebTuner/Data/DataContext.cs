0d2c61db15f2ce8a76d68e9289a57708377e8849
Вадим Якубовский <151447813+Vadim-Yakubowskij@users.noreply.github.com>
2024-06-09 15:34:30 +0300

    Mig

--- DemonLevTuner/WebTuner/Data/DataContext.cs
+++ DemonLevTuner/WebTuner/Data/DataContext.cs
@@ -1,8 +1,15 @@
-﻿namespace WebTuner.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebTuner.Data
 
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
 
+        }
+
+        public DbSet<RpgClass> rpg => Set<RpgClass>(); 
     }
 }
